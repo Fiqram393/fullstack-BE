fb794fa49015a9ed266f1dbb7f1aeb4bab5106f8
Fiqram393 <142194900+Fiqram393@users.noreply.github.com>
2023-12-10 13:15:30 +0700

    BE contact us

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { messageRoutes } = require("./routes/message.routes");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+	res.send("here is the response");
+});
+
+//messages routes
+app.use("/messages", messageRoutes);
+
+app.all("*", async (req, res) => {
+	res.json({
+		message: "Routes you're looking is not found",
+	});
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`Server is already running at ${PORT}`);
+});
